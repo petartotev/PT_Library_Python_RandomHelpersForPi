66fae5dc30eb8b4003a6d8e7f4fb98d3525d2e32
Petar Totev <petar@petartotev.net>
2022-03-22 11:56:48 +0200

    .py script implemented - reads movement of worm from game and visualizes it on matplotlib chart

--- projects/PythonUsingVisualStudio/pythotev_project_read_data_from_csv_file/pythotev_project_chart_visualization.py
+++ projects/PythonUsingVisualStudio/pythotev_project_read_data_from_csv_file/pythotev_project_chart_visualization.py
@@ -7,11 +7,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main():
+def read_lines_from_txt_file():
+    """
+    Read lines from a .txt file. No module needs to be imported.
+    """
+    file_t = open("res/t.txt")
+    lines_t = file_t.readlines()
+    line_t = lines_t[0]
+
+    file_x = open("res/x.txt")
+    lines_x = file_x.readlines()
+    line_x = lines_x[0]
+
+    return line_x, line_t
+
+
+def visualize_chart_something():
     plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 3, 6, 9, 10, 9, 8, 6, 7, 4])
-    plt.xlabel('x nums')
-    plt.ylabel('y nums')
+    plt.xlabel("x nums")
+    plt.ylabel("y nums")
     plt.show()
+
+
+def visualize_chart_movement_winner_worm_from_txt_files():
+    line_x, line_t = read_lines_from_txt_file()
+    plt.plot(line_t.split(', '), line_x.split(', '))
+    plt.xlabel("x nums")
+    plt.ylabel("t nums")
+    plt.show()
+
+
+def main():
+    # visualize_chart_something()
+    visualize_chart_movement_winner_worm_from_txt_files()
 
 
 if __name__ == "__main__":
