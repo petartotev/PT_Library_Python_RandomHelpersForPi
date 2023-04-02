369a8b596606520687388629e127c2405d6e1583
petartotev <petar@petartotev.net>
2023-04-02 20:56:04 +0300

    Refactored string to mp3 library file

--- libraries/pythotev_library_text_to_mp3.py
+++ libraries/pythotev_library_text_to_mp3.py
@@ -1,16 +1,20 @@
-from gtts import gTTS
+"""This Pythotev script reads a string and reads it in a mp3 file with gTTs library"""
+
 import os
+from gtts import gTTS
 
-def readStringInMp3File(string, directory, fileName):
-	if (directory[-1] != "/"):
-		directory = directory + "/"
-	if (".mp3" not in fileName):
-		fileName = fileName + ".mp3"
-	filePath = f'{directory}{fileName}'
-	defLang = 'en'
-	myObj = gTTS(text = string, lang = defLang, slow = False)
-	myObj.save(filePath)
-	return filePath
+def read_string_into_mp3_file(string, directory, file_name):
+    """This function reads a string into mp3 file"""
+    if directory[-1] != "/":
+        directory = directory + "/"
+    if ".mp3" not in file_name:
+        file_name = file_name + ".mp3"
+    file_path = f'{directory}{file_name}'
+    def_lang = 'en'
+    gtts_object = gTTS(text = string, lang = def_lang, slow = False)
+    gtts_object.save(file_path)
+    return file_path
 
-def playMp3File(filePath):
-	os.system(f'omxplayer -o alsa {filePath}')
+def play_mp3_file(file_path):
+    """This function plays a mp3 file using omxplayer"""
+    os.system(f'omxplayer -o alsa {file_path}')
